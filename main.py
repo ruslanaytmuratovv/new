648d42de8b7bcdbfac4b0d19f2b7d58273f17b61
sahonmila-sketch <sahonmila@gmail.com>
2026-03-14 17:44:04 +0500

    fdfds

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import os
 
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 import os
@@ -17,6 +17,28 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def start(message:Message):
     await message.answer(f"hello {message.from_user.first_name}")
+@dp.message(Command('help'))
+async def help(message:Message):
+    await message.answer('help ne jardem kerak')
+#2
+@dp.message(Command('conatct'))
+async def help(message:Message):
+    await message.answer('comn')
+#3
+@dp.message(Command('admin'))
+async def help(message:Message):
+    await message.answer('admin')
+#4
+@dp.message(Command('abaut'))
+async def help(message:Message):
+    await message.answer('abaud')
+#5
+@dp.message(Command('gmail'))
+async def help(message:Message):
+    await message.answer('gmail ne jardem')
+
+
+
 
 
 
